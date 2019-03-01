2c61503640af384811204d5d2322a6ef7593730f
Aditya Padhye <apadhye@adityas-mbp.lan>
2019-03-01 13:43:33 -0800

    Release 5.4.1

--- release.sh
+++ release.sh
@@ -1,9 +1,9 @@
-export GPDB_VERSION="5.10.2"
-export GPDB_INSTALLER="greenplum-db-5.10.2-rhel6-x86_64"
-export GPDB_INSTALLER_URL="https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/158026/product_files/191889/download"
+export GPDB_VERSION="5.4.1"
+export GPDB_INSTALLER="greenplum-db-5.4.1-rhel6-x86_64"
+export GPDB_INSTALLER_URL="https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/35017/product_files/66345/download"
 
-export MADLIB_INSTALLER="madlib-1.14-gp5-rhel6-x86_64"
-export MADLIB_INSTALLER_URL="https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/158026/product_files/191898/download"
+export MADLIB_INSTALLER="madlib-1.13-gp5-rhel6-x86_64"
+export MADLIB_INSTALLER_URL="https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/35017/product_files/66354/download"
 
 export DATA_SCIENCE_PYTHON_INSTALLER="DataSciencePython-1.1.1-gp5-rhel6-x86_64"
 export DATA_SCIENCE_PYTHON_INSTALLER_URL="https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/35017/product_files/66375/download"
